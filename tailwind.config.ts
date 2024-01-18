195b5657e923d2e9919165be5132db97d593c9db
Kgotso Phiri <kgotsokg12@gmail.com>
2024-01-18 21:14:24 +0200

    Tailwind Css theme setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,88 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
+      color: {
+        bg: {
+          300: "#c2c2c2",
+          200: "#ebebeb",
+          500: "#F5F5F5",
+        },
+        primary: {
+          300: "#e0ffff",
+          200: "#69a9ff",
+          500: "#007BFF",
+        },
+        accent: {
+          200: "#916400",
+          500: "FFC107",
+        },
+        dark: {
+          100: "#000000",
+          200: "#0F1117",
+          300: "##151821",
+          400: "#212734",
+          500: "#3F4354",
+        },
+        light: {
+          400: "#858EAD",
+          500: "#7B8EC8",
+          700: "#DCE3F1",
+          800: "#F4F6F8",
+          850: "#FDFDFD",
+          900: "#FFFFFF",
+        },
+      },
+      fontFamily: {
+        //Add typography font for maths/physics
+        inter: ["var(--font-inter)"],
+        spaceGrotesk: ["var(--font-spaceGrotesk)"], 
+      },
+      boxShadow: {
+        "light-100":
+          "0px 12px 20px 0px rgba(184, 184, 184, 0.03), 0px 6px 12px 0px",
+        "light-200": "10px 10px 20px 0px rgba(218, 213, 213, 0.10)",
+        "light-300": "-10px 10px 20px 0px rgba(218, 213, 213, 0.10)",
+        "dark-100": "0px 2px 10px 0px rgba(46, 52, 56, 0.10)",
+        "dark-200": "2px 0px 20px 0px rgba(39, 36, 36, 0.04)",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "auth-dark": "url('/assets/images/auth-dark.png')",
+        "auth-light": "url('/assets/images/auth-light.png')",
+      },
+      screens: {
+        xs: "320px",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require('tailwindcss-animate'),require('@tailwindcss/typography')],
+};
 export default config
