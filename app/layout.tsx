392ca9feb911ac840e0feb8ba2b14229f9f40740
Kgotso Phiri <kgotsokg12@gmail.com>
2024-01-21 17:53:20 +0200

    YAAS-KS: Add themeProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,12 @@ import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/context/ThemeProvider";
 
 const inter = Inter({
   subsets: ["latin"],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  variable: '--font-inter'
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
 });
 
 export const metadata: Metadata = {
@@ -14,28 +15,30 @@ export const metadata: Metadata = {
   description:
     "Welcome to the Collaborative Teaching Hub – web app empowering educators to collaborate and elevate teaching. Share knowledge, methods, and resources, fostering a vibrant community of learning.",
   icons: {
-    icon: '/assets/icons/knowledge-share-logo-icon.svg'
-  }
+    icon: "/assets/icons/knowledge-share-logo-icon.svg",
+  },
 };
 
 export default function RootLayout({
-  children, 
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider
-      appearance={{
-        elements: {
-          formButtonPrimary: 'primary-gradient',
-          footerActionLink: 'primary-text-gradient hover:text-primary-500'
-      }
-    }}
-    >
-      <html lang="en">
-        <h1 className="h1-bold">This is a text</h1>
-        <body className={`${inter.variable}`}>{children}</body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <h1 className="h1-bold">This is a text</h1>
+      <body className={`${inter.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: "primary-gradient",
+              footerActionLink: "primary-text-gradient hover:text-primary-500",
+            },
+          }}
+        >
+          <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
